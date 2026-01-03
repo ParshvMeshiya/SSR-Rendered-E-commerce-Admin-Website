a4cd15e8e6fba6560a4233f994529751e3669770
ParshvMeshiya <parshv11meshiya@gmail.com>
2026-01-03 15:57:58 +0530

    done

--- middleware.js
+++ middleware.js
@@ -15,7 +15,7 @@ export function middleware(request) {
   if (token && role !== "admin" && isAdminRoute) {
     return NextResponse.redirect(new URL("/", request.url));
   }
-  if (token && pathname === "/") {
+  if (token && role === "admin" && isAuthPage) {
     return NextResponse.redirect(new URL("/dashboard", request.url));
   }
   return NextResponse.next();
